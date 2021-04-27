3b3f98b2b4bcb6bfe2e36858f73118aa368bdc8a
Karolina Okla <karolinaokla95@gmail.com>
2021-04-27 19:26:25 +0200

    Add sendMessage function

--- client/app.js
+++ client/app.js
@@ -20,4 +20,31 @@ const login = function(event) {
     } else window.alert('Please write your login.');
 };
 
+
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+  
+  
+
+const sendMessage = function(event) {
+    event.preventDefault();
+    if(references.messageContentInput.value) {
+        addMessage(userName, references.messageContentInputt.value);
+        references.messageContentInput.value = '';
+    } else window.alert('You have to type your message');
+}
+
 references.loginForm.addEventListener('submit', login);
+references.addMessageForm.addEventListener('submit', sendMessage);
